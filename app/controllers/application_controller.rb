039b3ead2b15427ae9624fa0327824717dd33cd9
erijawa <e.akgw0913@gmail.com>
2024-09-24 10:52:05 +0900

    Add:Sorceryでログイン機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :require_login
   allow_browser versions: :modern
+
+  private
+
+  def not_authenticated
+    redirect_to login_path
+  end
 end
