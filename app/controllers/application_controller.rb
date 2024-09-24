f494456e2e92bf51f8a9a3832ca0699b5873b741
erijawa <e.akgw0913@gmail.com>
2024-09-24 22:37:30 +0900

    Add:flashmessageの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :require_login
   allow_browser versions: :modern
+  add_flash_types :success, :danger
 
   private
 
